d222d21879c2ebe52df1c00fb17c77300db59307
zhongyiliang <zhongyiliang@163.com>
2017-11-07 22:15:36 +0800

    增加service-trubine，service-hihi，consul-miya模块

---
+++ mircoboot/consul-miya/src/main/java/org/consul/miya/ConsulMiyaApplication.java
@@ -0,0 +1,21 @@
+package org.consul.miya;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class ConsulMiyaApplication  {
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ConsulMiyaApplication.class).web(true).run(args);
+	}
+	
+	@RequestMapping("/hi")
+    public String home() {
+        return "hi ,i'm miya";
+    }
+}
